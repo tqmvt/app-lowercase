cf56f92f875da0cc29ad8fb2b0802995ba5e74d4
tqmvt <66651502+tqmvt@users.noreply.github.com>
2024-04-03 18:36:29 -0700

    chore: update content

--- config/site.ts
+++ config/site.ts
@@ -1,18 +1,18 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "tqmvt",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Lowercase your words",
   mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
+    // {
+    //   title: "Home",
+    //   href: "/",
+    // },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/_scentforest",
+    github: "https://github.com/tqmvt",
+    // docs: "https://ui.shadcn.com",
   },
 }
